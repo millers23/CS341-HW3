603d5f01e4d31b7e3ec4f8ae0d82767b3000b5c2
millers23 <millers23@up.edu>
2022-02-19 16:20:33 -0800

    iterating on a different idea, need POST in main.js

--- hw3/public/javascripts/main.js
+++ hw3/public/javascripts/main.js
@@ -10,48 +10,56 @@ function validateForm() {
     }
 }
 
+let cherry = 11
+let chocolate = 17
+let plain = 31
 //https://stackoverflow.com/questions/692196/post-request-javascript
-function getMonth( url, params, method ){
+function getMonth(month){
+    if (month == 0) {
 
-    params = params || {};
-    method = method || "post";
-
-    // function to remove the iframe
-    var removeIframe = function( iframe ){
-        iframe.parentElement.removeChild(iframe);
-    };
-
-    // make a iframe...
-    var iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-
-    iframe.onload = function(){
-        var iframeDoc = this.contentWindow.document;
-
-        // Make a invisible form
-        var form = iframeDoc.createElement('form');
-        form.method = method;
-        form.action = url;
-        iframeDoc.body.appendChild(form);
-
-        // pass the parameters
-        for( var cherry,chocolate,plain in params ){
-            var input = iframeDoc.createElement('input');
-            input.type = 'hidden';
-            input.numCherry = params[cherry];
-            input.numChocolate = params[chocolate];
-            input.numPlain = params[plain];
-            form.appendChild(input);
-        }
-
-        form.submit();
-        // remove the iframe
-        setTimeout( function(){ 
-            removeIframe(iframe);
-        }, 500);
-    };
-
-    document.body.appendChild(iframe);
+    }
+    if (month == 1) {
+        
+    }
+    if (month == 2) {
+        
+    }
+    if (month == 3) {
+        
+    }
+    if (month == 4) {
+        
+    }
+    if (month == 5) {
+        
+    }
+    if (month == 6) {
+        
+    }
+    if (month == 7) {
+        
+    }
+    if (month == 8) {
+        
+    }
+    if (month == 9) {
+        
+    }
+    if (month == 10) {
+        
+    }
+    if (month == 11) {
+        
+    }
+}
+function getCherry(){
+    return "Cherry: "+cherry;
+}
+function getChocolate(){
+    return "Chocolate: "+chocolate;
+}
+function getPlain(){
+    return "Plain: "+plain;
 }
 
 //https://stackoverflow.com/questions/37189306/javascript-bootstrap-dropdown-on-hover-open-all-menu-levels-of-submenu
